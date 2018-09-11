6008b0da91509b08ef4db72ba4b69d73ed6a1996
Alessandro Vendruscolo <alessandro.vendruscolo@gmail.com>
2018-09-11 17:14:44 +0200

    Add a different custom gauge

--- Example/Gauge/ViewController.swift
+++ Example/Gauge/ViewController.swift
@@ -21,12 +21,29 @@ class ViewController: UIViewController {
         defaultGauge.width(320)
         defaultGauge.height(320)
 
+        view.addSubview(gauge2)
+        gauge2.bottomToSuperview()
+        gauge2.trailing(to: view)
+        gauge2.width(320)
+        gauge2.height(320)
+
     }
 
     private lazy var defaultGauge: Gauge = {
         let g = Gauge(bindingBehaviour: .title)
         g.range = 0...100
         g.value = 50
+
+        return g
+    }()
+
+    private lazy var gauge2: Gauge = {
+        let g = Gauge(bindingBehaviour: .none)
+        g.range = 0...100
+        g.value = 100
+        g.emptyBottomSliceAngle = 0
+        g.minValueLabel.isHidden = true
+        g.maxValueLabel.isHidden = true
 
         return g
     }()
